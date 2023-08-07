b61ff89054f2d069e111af50647ebb13f5106851
md606 <jimmy_6905@outlook.com>
2023-08-07 15:01:45 +0800

    first commit

---
+++ WpfApp1/CheckNextMusic.cs
@@ -0,0 +1,274 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    public static class CheckNextMusic
+    {
+        public static void Check(out int NextHour, out int NextMinute, out string NextMusic)
+        {
+            int NowHour = DateTime.Now.Hour;
+            int NowMinute = DateTime.Now.Minute;
+            string Pre = "PrepareMusic.mp3", Start = "StartMusic.mp3", End = "EndMusic.mp3", YBJC = "YBJCMusic.mp3", Pao = "PaoCaoMusic.mp3", Sleep = "SleepMusic.mp3", Wake = "WakeMusic.mp3";
+            Console.WriteLine(NowHour);
+            Console.WriteLine(NowMinute);
+            if (NowHour < 8) // 分为Hour<8, Hour<13, Hour<17 几个大节点内再细分
+            {
+                if (NowHour < 7)
+                {
+                    NextHour = 7;
+                    NextMinute = 58;
+                    NextMusic = Pre;
+                }
+                else // NowHour == 7
+                {
+                    if (NowMinute < 58)
+                    {
+                        NextHour = 7;
+                        NextMinute = 58;
+                        NextMusic = Pre;
+                    }
+                    else
+                    {
+                        NextHour = 8;
+                        NextMinute = 00;
+                        NextMusic = Start;
+                    }
+                }
+            }
+            else if (NowHour < 13) // from 8:00 to 12:59 // 五节课+眼保健操+大课间+午休铃
+            {
+                if (NowHour < 10)
+                {
+                    if (NowHour == 8)
+                    {
+                        if (NowMinute < 40)
+                        {
+                            NextHour = 8;
+                            NextMinute = 40;
+                            NextMusic = End;
+                        }
+                        else if (NowMinute < 41)
+                        {
+                            NextHour = 8;
+                            NextMinute = 41;
+                            NextMusic = YBJC;
+                        }
+                        else if (NowMinute < 48)
+                        {
+                            NextHour = 8;
+                            NextMinute = 48;
+                            NextMusic = Pre;
+                        }
+                        else if (NowMinute < 50)
+                        {
+                            NextHour = 8;
+                            NextMinute = 50;
+                            NextMusic = Start;
+                        }
+                        else
+                        {
+                            NextHour = 9;
+                            NextMinute = 30;
+                            NextMusic = End;
+                        }
+                    }
+                    else // NowHour == 9
+                    {
+                        if (NowMinute < 30)
+                        {
+                            NextHour = 9;
+                            NextMinute = 30;
+                            NextMusic = End;
+                        }
+                        else if (NowMinute < 38)
+                        {
+                            NextHour = 9;
+                            NextMinute = 38;
+                            NextMusic = Pre;
+                        }
+                        else if (NowMinute < 40)
+                        {
+                            NextHour = 9;
+                            NextMinute = 40;
+                            NextMusic = Start;
+                        }
+                        else
+                        {
+                            NextHour = 10;
+                            NextMinute = 20;
+                            NextMusic = End;
+                        }
+                    }
+                }
+                else // NowHour >= 10
+                {
+                    if (NowHour == 10)
+                    {
+                        if (NowMinute < 20)
+                        {
+                            NextHour = 10;
+                            NextMinute = 20;
+                            NextMusic = End;
+                        }
+                        else if (NowMinute < 21)
+                        {
+                            NextHour = 10;
+                            NextMinute = 21;
+                            NextMusic = Pao;
+                        }
+                        else if (NowMinute < 48)
+                        {
+                            NextHour = 10;
+                            NextMinute = 48;
+                            NextMusic = Pre;
+                        }
+                        else if (NowMinute < 50)
+                        {
+                            NextHour = 10;
+                            NextMinute = 50;
+                            NextMusic = Start;
+                        }
+                        else
+                        {
+                            NextHour = 11;
+                            NextMinute = 30;
+                            NextMusic = End;
+                        }
+                    }
+                    else if (NowHour == 11)
+                    {
+                        if (NowMinute < 30)
+                        {
+                            NextHour = 11;
+                            NextMinute = 30;
+                            NextMusic = End;
+                        }
+                        else if (NowMinute < 38)
+                        {
+                            NextHour = 11;
+                            NextMinute = 38;
+                            NextMusic = Pre;
+                        }
+                        else if (NowMinute < 40)
+                        {
+                            NextHour = 11;
+                            NextMinute = 40;
+                            NextMusic = Start;
+                        }
+                        else
+                        {
+                            NextHour = 12;
+                            NextMinute = 20;
+                            NextMusic = End;
+                        }
+                    }
+                    else // NowHour == 12
+                    {
+                        if (NowMinute < 20)
+                        {
+                            NextHour = 12;
+                            NextMinute = 20;
+                            NextMusic = End;
+                        }
+                        else
+                        {
+                            NextHour = 13;
+                            NextMinute = 00;
+                            NextMusic = Sleep;
+                        }
+                    }
+                }
+            }
+            else if (NowHour < 17)
+            {
+                if (NowHour == 13)
+                {
+                    NextHour = 14;
+                    NextMinute = 00;
+                    NextMusic = Wake;
+                }
+                else if (NowHour == 14)
+                {
+                    if (NowMinute < 5)
+                    {
+                        NextHour = 14;
+                        NextMinute = 05;
+                        NextMusic = Pre;
+                    }
+                    else if (NowMinute < 20)
+                    {
+                        NextHour = 14;
+                        NextMinute = 20;
+                        NextMusic = Start;
+                    }
+                    else
+                    {
+                        NextHour = 15;
+                        NextMinute = 00;
+                        NextMusic = End;
+                    }
+                }
+                else if (NowHour == 15)
+                {
+                    if (NowMinute < 1)
+                    {
+                        NextHour = 15;
+                        NextMinute = 01;
+                        NextMusic = YBJC;
+                    }
+                    else if (NowMinute < 18)
+                    {
+                        NextHour = 15;
+                        NextMinute = 18;
+                        NextMusic = Pre;
+                    }
+                    else if (NowMinute < 20)
+                    {
+                        NextHour = 15;
+                        NextMinute = 20;
+                        NextMusic = Start;
+                    }
+                    else
+                    {
+                        NextHour = 16;
+                        NextMinute = 00;
+                        NextMusic = End;
+                    }
+                }
+                else // NowHour == 16
+                {
+                    if (NowMinute < 8)
+                    {
+                        NextHour = 16;
+                        NextMinute = 8;
+                        NextMusic = Pre;
+                    }
+                    else if (NowMinute < 10)
+                    {
+                        NextHour = 16;
+                        NextMinute = 10;
+                        NextMusic = Start;
+                    }
+                    else if (NowMinute < 50)
+                    {
+                        NextHour = 16;
+                        NextMinute = 50;
+                        NextMusic = End;
+                    }
+                    else
+                    {
+                        NextHour = 7;
+                        NextMinute = 58;
+                        NextMusic = Pre;
+                    }
+                }
+            }
+            else // NowHour >=17
+            {
+                NextHour = 7;
+                NextMinute = 58;
+                NextMusic = Pre;
+            }
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        MediaPlayer player = new MediaPlayer();
+        int NextHour, NextMinute;
+        string NextMusic, NextMusicString;
+        bool IsChecked=false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TimeSpan.FromSeconds(0.1);   //设置刷新的间隔时间
+            timer.Start();
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            nowTime.Text = string.Concat("当前时间: ", DateTime.Now.ToString("HH:mm:ss"));
+            if (!IsChecked)
+            {
+                CheckNextMusic.Check(out NextHour, out NextMinute, out NextMusic);
+                if (NextMusic == "PrepareMusic.mp3") NextMusicString = "预备铃";
+                else if (NextMusic == "StartMusic.mp3") NextMusicString = "上课铃";
+                else if (NextMusic == "EndMusic.mp3") NextMusicString = "下课铃";
+                else if (NextMusic == "YBJCMusic.mp3") NextMusicString = "眼保健操";
+                else if (NextMusic == "PaoCaoMusic.mp3") NextMusicString = "跑操";
+                else if (NextMusic == "SleepMusic.mp3") NextMusicString = "午休铃";
+                else NextMusicString = "起床铃";
+                nextRing.Text = $"下个铃声: {NextMusicString}";
+                nextTime.Text = $"响铃时间: {NextHour}:{NextMinute}";
+                Console.WriteLine(NextHour);
+                Console.WriteLine(NextMinute);
+                Console.WriteLine(NextMusic);
+                IsChecked = true;
+            }
+            if (DateTime.Now.Hour == NextHour && DateTime.Now.Minute == NextMinute)
+            {
+                player.Open(new Uri($"./assets/{NextMusic}", UriKind.Relative));
+                player.Play();
+            }
+        }
+
+        //private void TestButton_Click(object sender, RoutedEventArgs e) // 测试按钮 勿删
+        //{
+        //    player.Open(new Uri("./assets/EndMusic.mp3", UriKind.Relative)); // 成功播放mp3 StartMusic设置为始终复制,内容
+        //    player.Play();
+        //}
+    }
+}
